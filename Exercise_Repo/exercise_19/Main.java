98d969a193d8a9e7ae8510ccb5323bb2c74ff723
SLroot <saeid17lotfi@gmail.com>
2026-09-17 02:46:04 +0330

    some exercises

---
+++ Exercise_Repo/exercise_19/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SumOfASequenceTheSequel {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
+
+        System.out.println("First number?");
+        int firtsNumber = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Second number?");
+        int secondNumber = Integer.valueOf(scanner.nextLine());
+
+        for (int i = firtsNumber; i <= secondNumber; i++) {
+            sum = sum + i;
+        }
+
+        System.out.println("The sum is: " + sum);
+    }
+}
